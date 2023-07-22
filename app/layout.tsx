b0ee43f73f37a04061fd1aa557c0b86312275fb4
Ali <mailme.ali2004@gmail.com>
2023-07-22 00:28:15 -0400

    sidebar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar";
 import "@/styles/globals.css";
 
 export const metadata = {
@@ -12,10 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex bg-[#F7FBFF] h-screen">
         {/* Sidebar */}
-
-        <main>
+        <Sidebar />
+        <main className="p-10 max-w-7xl w-full mx-auto overflow-y-auto">
           {/* Header */}
           {children}
         </main>
